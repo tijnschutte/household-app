806b0c78c0cb59bd939672ae7d529b9a07148b8a
Tijn Schutte <64611459+tijnschutte@users.noreply.github.com>
2025-03-04 13:24:31 +0100

    fixed build issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import prisma from '@/lib/prisma';
+import { User } from '@prisma/client';
 
 export default async function Page() {
-    const users = await prisma.user.findMany();
+    const users: User[] = await prisma.user.findMany();
     return (
         <ol className="list-decimal list-inside font-[family-name:var(--font-geist-sans)]">
             {users.map((user) => (
